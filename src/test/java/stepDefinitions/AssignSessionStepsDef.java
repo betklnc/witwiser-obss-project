03644a2e1552a82839c1dd3f00cdc37442403521
Betul Kabadayi <betul.kabadayi@obss.com.tr>
2023-01-17 01:50:23 +0300

    AssignSessionStepsDef refactored.

--- src/test/java/stepDefinitions/AssignSessionStepsDef.java
+++ src/test/java/stepDefinitions/AssignSessionStepsDef.java
@@ -9,39 +9,39 @@ import pageObject.SessionPageObject;
 
 public class AssignSessionStepsDef extends BasePageObject {
 
-    private LoginPageObject login_po;
-    private SessionPageObject session_po;
+    private LoginPageObject loginPo;
+    private SessionPageObject sessionPo;
 
-    public AssignSessionStepsDef(LoginPageObject login_po, SessionPageObject session_po) {
-        this.login_po = login_po;
-        this.session_po = session_po;
+    public AssignSessionStepsDef(LoginPageObject loginPo, SessionPageObject sessionPo) {
+        this.loginPo = loginPo;
+        this.sessionPo = sessionPo;
     }
 
     @Given("User logins to Witwiser system with {} and {}")
     public void user_logins_to_witwiser_system(String username, String password) {
-        login_po.navigateToWitwiserUrl();
-        login_po.setUsername(username);
-        login_po.setPassword(password);
-        login_po.clickOnLoginButton();
+        loginPo.navigateToWitwiserUrl();
+        loginPo.setUsername(username);
+        loginPo.setPassword(password);
+        loginPo.clickOnLoginButton();
     }
 
     @When("User assigns session to user")
     public void user_assigns_session_to_user() {
-        session_po.clickOnSessionLink();
-        session_po.clickOnAssignSessionButton();
-        session_po.clickOnTemplate();
-        session_po.clickOnTemplateOption();
-        session_po.clickOnStartDatePickerInputAndSelectCurrentDate();
-        session_po.clickOnEndDatePickerInputAndSelectCurrentDate();
-        session_po.clickOnAssignmentTypeSelection();
-        session_po.clickOnAssignmentTypeOption();
-        session_po.clickOnTestTakerAndSetUser();
-        session_po.clickOnAssignSessionFormButton();
+        sessionPo.clickOnSessionLink();
+        sessionPo.clickOnAssignSessionButton();
+        sessionPo.clickOnTemplate();
+        sessionPo.clickOnTemplateOption();
+        sessionPo.clickOnStartDatePickerInputAndSelectCurrentDate();
+        sessionPo.clickOnEndDatePickerInputAndSelectCurrentDate();
+        sessionPo.clickOnAssignmentTypeSelection();
+        sessionPo.clickOnAssignmentTypeOption();
+        sessionPo.clickOnTestTakerAndSetUser();
+        sessionPo.clickOnAssignSessionFormButton();
     }
 
     @Then("It's seen that user complete assigning session")
     public void it_s_seen_that_user_complete_assigning_session() {
-        session_po.validateCompleteAssignSession();
+        sessionPo.validateCompleteAssignSession();
     }
 
 }
